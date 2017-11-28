e8c86fb0c9648a93652b55bb122a3c21913b24e1
Alexey Kozeka <kozeka.alex@gmail.com>
2017-11-28 16:53:08 +0200

    Added confirmation modal for delete action.

--- web/js/app.js
+++ web/js/app.js
@@ -6,4 +6,26 @@ function initUI() {
     $('button.btn-back').click(function () {
         window.history.back();
     });
+
+    $(document).on('submit', 'form[name="delete_entity"]', function (e) {
+        var $form = $(this);
+
+        if ($form.data('confirmed')) {
+            return;
+        }
+
+        e.preventDefault();
+
+        eModal
+            .confirm({
+                message: 'Are you sure you want to delete?',
+                title: 'Delete confirmation',
+                size: eModal.size.sm
+            })
+            .then(function () {
+                $form.data('confirmed', true);
+                $form.submit();
+            })
+        ;
+    });
 }
